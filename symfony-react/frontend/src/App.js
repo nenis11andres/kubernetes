6fb5230d858eb0007cdffa9c46b6137e6862789d
nenis11andres <nenis11andres@gmail.com>
2026-03-03 13:22:18 +0100

    Modificamos todos los archivos necesarios para comprobar que funciona el despliegue en minikube

--- symfony-react/frontend/src/App.js
+++ symfony-react/frontend/src/App.js
@@ -1,24 +1,22 @@
 import { useEffect, useState } from 'react';
 
-
 function App() {
-const [message, setMessage] = useState('');
+    const [message, setMessage] = useState('');
 
+    useEffect(() => {
+        // Llamamos directamente a la ruta relativa
+        fetch('/api/hello')
+            .then(r => r.json())
+            .then(d => setMessage(d.message))
+            .catch(console.error);
+    }, []);
 
-useEffect(() => {
-fetch('http://localhost:8000/api/hello')
-.then(res => res.json())
-.then(data => setMessage(data.message));
-}, []);
-
-
-return (
-<div style={{ padding: 20 }}>
-<h1>React + Symfony</h1>
-<p>{message}</p>
-</div>
-);
+    return (
+        <div style={{ padding: 20 }}>
+            <h1>React + Symfony</h1>
+            <p>{message}</p>
+        </div>
+    );
 }
-
 
 export default App;
